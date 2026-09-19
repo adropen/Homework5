f19564df7a048228e7dd0bca019ac2cc9f6844d5
Артур <artur_kolm@mail.ru>
2026-09-19 12:53:18 +0800

    Переделал

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ void main() {
     int clientOS = 0;
     if (clientOS == 0) {
         System.out.println("Установите версию приложения для iOS по ссылке");
-    } else if (clientOS == 1) {
+    } else {
         System.out.println("Установите версию приложения для Android по ссылке");
 
     }
